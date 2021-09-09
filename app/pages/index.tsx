2bc97e7c59591fb057b7afd36c12124a4e8841b3
Misato Maeda <byfarsmall@gmail.com>
2021-09-10 01:16:55 +0900

    test tailswind

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 
 const Home: FC = () => {
-  return <div className="text-3xl">hoge</div>;
+  return <div className="text-9xl">hoge</div>;
 };
 
 export default Home;
